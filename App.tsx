92374f7378ba6071b2ca8d9269d1a43b1203ecff
Bassem Dimassi <dimassibassem99@gmail.com>
2022-10-03 22:42:16 +0100

    First React Native Application

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import Tasks from "./components/Tasks";
+import "./app.d";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View className="flex-1 bg-red-500">
+            <View style={styles.tasksWrapper}>
+                <Text style={styles.sectionTitle}>Today Tasks</Text>
+                <View style={styles.items}>
+                    <Tasks task={"task1"}/>
+                    <Tasks task={"task2"}/>
+                    <Tasks task={"task3"}/>
+                </View>
+            </View>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    tasksWrapper: {
+        paddingTop: 80,
+        paddingHorizontal: 20,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    items: {
+        marginTop: 30,
+    }
 });
